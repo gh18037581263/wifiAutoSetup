d2cfc6b2ca9754311542f91e481e8576fd4ecbc0
Yu YongZhen <yuyz@rock-chips.com>
2018-09-14 15:37:13 +0800

    EasySetup: update to EasySetup4.2 from Ampak
    corresponding apk:
    external/app/EasySetupAndroid-v2.2.0.zip
    external/EasySetupIOS-v2.1.0.zip
    
    Change-Id: If5df3b2c1dc89056645e68360df67514bb4f4229
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>

--- Socket.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOCKET_H
-#define SOCKET_H
-
-int socket_connect(char *ip, int port);
-int socket_send(char *msg);
-void socket_close();
-
-#endif
--- easy_setup_linux.c
+++ easy_setup_linux.c
@@ -10,9 +10,9 @@
 #include <easy_setup.h>
 #include <scan.h>
 
-#define ES_TARGET_VERSION "v3.7.0"
+#define ES_TARGET_VERSION "v4.0.0"
 
-#define QUERY_TIMEOUT_MS (30*1000)
+//#define QUERY_TIMEOUT_MS (30*1000) /* replaced with easy_setup_timeout_s */
 #define QUERY_INTERVAL_MS (1000)
 
 #define WLAN_IFACE "wlan0"
@@ -23,7 +23,7 @@
 #define WLC_GET_VAR (262)
 #define WLC_SET_VAR (263)
 
-#define DEFAULT_PROTOCOL_MASK (0x3) /* bcast */
+#define DEFAULT_PROTOCOL_MASK (0x3) /* mcast */
 uint16 g_protocol_mask = 0; /* default no protocols enabled */
 
 /* Linux network driver ioctl encoding */
@@ -41,6 +41,7 @@ static uint8 g_debug = 0;
 static easy_setup_result_t g_result;
 static uint8 g_protocol = 0;
 extern int killed;
+static int easy_setup_timeout_s = 60; /* -1 stands for infinity */
 
 int easy_setup_start() {
     int i;
@@ -90,7 +91,7 @@ int easy_setup_stop() {
     int ret = 0;
 
     if (g_ioc_fd < 0) {
-        //LOGE("easy setup query: control socket not initialized.\n");
+        LOGE("easy setup query: control socket not initialized.\n");
         return -1;
     }
 
@@ -112,7 +113,7 @@ int easy_setup_stop() {
 /* query result is of tlv format
  * type = protocol shot
  * length = result length
- * value = bcast_result_t/akiss_result_t ... */
+ * value = mcast_result_t/akiss_result_t ... */
 int easy_setup_query() {
     int ret = 0;
     tlv_t* query = NULL;
@@ -130,8 +131,8 @@ int easy_setup_query() {
     }
     
     uint last_state = 0;
-    int tries = QUERY_TIMEOUT_MS/QUERY_INTERVAL_MS;
-    while (tries-- && !killed) {
+    int total_time_ms = 0;
+    while (!killed) {
         usleep(QUERY_INTERVAL_MS * 1000);
 
         ret = easy_setup_iovar(0, query, RESULT_MAX_LEN);
@@ -155,12 +156,15 @@ int easy_setup_query() {
                 }
             }
         }
-    }
 
-    if (tries <= 0) {
-        LOGE("easy setup query timed out.\n");
-        free(query);
-        return -1;
+        if (easy_setup_timeout_s > 0) {
+            total_time_ms += QUERY_INTERVAL_MS;
+            if (total_time_ms >= easy_setup_timeout_s*1000) {
+                LOGE("timeout (%d)\n", easy_setup_timeout_s);
+                free(query);
+                return -1;
+            }
+        }
     }
 
     free(query);
@@ -224,6 +228,10 @@ int easy_setup_ioctl(int cmd, int set, void* param, int size) {
     }
 
     return 0;
+}
+
+void easy_setup_set_timeout(int s) {
+    easy_setup_timeout_s = s;
 }
 
 int easy_setup_get_ssid(char buff[], int buff_len) {
